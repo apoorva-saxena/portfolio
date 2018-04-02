d6c15998a53d92edee20c88d325aeaeb60db5f0e
Apoorva Saxena <apsaxena24@gmail.com>
2018-04-02 23:36:14 +0100

    Adds the structure for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './images/logo.png';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './images/logo.png'
+import './App.css'
+import {Row} from 'react-bootstrap/lib/Row'
+import {Col} from 'react-bootstrap/lib/Col'
+import {Grid} from 'react-bootstrap/lib/Grid'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous"/>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/questions" component={Question}/>
+            <Route exact path="/sign-in" component={SignIn}/>
+            <Route exact path="/sign-up" component={SignUp}/>
+            {/* <Route path="/:name" component={Category}/> */}
+          </Switch>
+        </Router>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Apoorva Saxena</h1>
-          <div>
-            <span>About  </span>
+          <div> */}
+            {/* <Grid>
+              <Row className="show-grid">
+                <Col xs={6} md={4}>
+                  <code>&lt;{'Col xs={6} md={4}'} /">&gt;</code>
+                </Col>
+                <Col xs={6} md={4}>
+                  <code>&lt;{'Col xs={6} md={4}'} /">&gt;</code>
+                </Col>
+                <Col xsHidden md={4}>
+                  <code>&lt;{'Col xsHidden md={4}'} /">&gt;</code>
+                </Col>
+              </Row>
+            </Grid> */}
+            {/* <span>About  </span>
             <span>CV  </span>
             <span>Github Projects  </span>
-            <span>LinkedIN  </span>
-          </div>
-        </header>
-        <p className="App-intro">
-        </p>
+            <span>LinkedIN  </span> */}
+          {/* </div> */}
+        {/* // </header> */}
+        {/* // <p className="App-intro"></p> */}
       </div>
-    );
+    )
   }
 }
 
